280f66cbd6098f96ea6355dbd846abb20417e952
Dan Piponi <dpiponi@gmail.com>
2018-08-23 21:21:56 -0700

    Small

--- examples/ex5/Main.hs
+++ examples/ex5/Main.hs
@@ -15,11 +15,7 @@ import Lines
 import Triangles
 import Data.Array
 import Debug.Trace
-
-clamp :: Float -> Float -> Float -> Float
-clamp a b x | x < a = a
-            | x > b = b
-            | otherwise = x
+import Turtle
 
 lerp :: Float -> Float -> Float -> Float
 lerp a b x = (1-x)*a+x*b
@@ -277,13 +273,13 @@ main = do
 
         let f t = (t, 0.25*t*t)
         let time' = 0.3*time
-        let d = 3*(time'-fromIntegral @Int (floor time'))
+--         let d = 3*(time'-fromIntegral @Int (floor time'))
 --         lift $ plotPath 100 f 0 d
 --        let curve = integrate (\x y ->let (u,v)=interpVelocityField vx vy x y in (10*u,10*v)) 0 0 100 0.01
 --         let curve = integrate (\x y ->let (u,v)=interpVelocityField vx vy x y in (u,v)) 1.0 1.0 300 0.01
         forM_ [-5.0, -3.0..5.0] $ \x -> do
             forM_ [-5.0, -3.0..5.0] $ \y -> do
                 let curve = integrate (interpVelocityField vx vy) (10+x) (10+y) 500 0.1
-                lift $ drawPath (0.25, 1.0, 0.25) [(0.1*u, 0.1*v) | (u, v) <- curve]
+                lift $ drawPath (0.25, 1.0, 0.25) [(0.1*pu, 0.1*pv) | (pu, pv) <- curve]
 
         return ()
